34fc2ca16df9e7c5a2c6dc41b947c69bd350e161
Viktor Lazarev <taurus101v@gmail.com>
2013-08-07 01:02:17 +0400

    ExtractionContainers

--- src/gentoid/route/ExtractionContainers.php
+++ src/gentoid/route/ExtractionContainers.php
@@ -6,9 +6,28 @@ namespace gentoid\route;
 class ExtractionContainers {
 
 	/** @var \gentoid\route\NodeID[] */
-	protected $nodeIDs = array();
+	protected $usedNodeIDs = array();
 
 	/** @var \gentoid\route\Node[] */
-	protected $nodes = array();
+	protected $allNodes = array();
+
+	/** @var \gentoid\route\InternalExtractorEdge[] */
+	protected $allEdges = array();
+
+	/** @var string[] */
+	protected $nameVector = array();
+
+	/** @var \gentoid\route\RawRestrictionContainer[] */
+	protected $restrictionsVector = array();
+
+	/** @var \gentoid\route\WayIDStartAndEndEdge[] */
+	protected $wayStartEndVector = array();
+
+	public function prepareData($outputFileName, $restrictionsFileName) {
+		$usedNodeCounter = 0;
+		$usedEdgeCounter = 0;
+
+		usort($this->usedNodeIDs, array('NodeID', 'cmp'));
+	}
 
 }
