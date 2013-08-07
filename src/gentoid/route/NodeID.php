221bae9fb6e524300c26dd0d07d1933582d570bd
Viktor Lazarev <taurus101v@gmail.com>
2013-08-07 09:46:43 +0400

    Update class NodeID

--- src/gentoid/route/NodeID.php
+++ src/gentoid/route/NodeID.php
@@ -5,6 +5,12 @@ namespace gentoid\route;
 
 class NodeID extends BigInt {
 
+	const DEFAULT_VALUE = '-1';
+
+	public function __construct($value = NodeID::DEFAULT_VALUE) {
+		parent::__construct($value);
+	}
+
 	/**
 	 * @param NodeID $a
 	 * @param NodeID $b
@@ -12,6 +18,31 @@ class NodeID extends BigInt {
 	 */
 	public static function cmp(NodeID $a, NodeID $b) {
 		return bccomp($a->getValue(), $b->getValue());
+	}
+
+	/**
+	 * @param NodeID[] $array
+	 * @throws \Exception
+	 */
+	public static function unique(array &$array) {
+		$ids = array();
+
+		foreach ($array as $key => $nodeId) {
+			if (!($nodeId instanceof NodeID)) {
+				throw new \Exception('Object is not instance of NodeID');
+			}
+			$id = $nodeId->getValue();
+			if (isset($ids[$id])) {
+				unset($array[$key]);
+			}
+			else {
+				$ids[$id] = 1;
+			}
+		}
+	}
+
+	public function pack() {
+		return pack('a20', $this->value);
 	}
 
 }
