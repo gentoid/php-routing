8f6d0a37caf3a309a8b98cf972049a3426fd9df5
Viktor Lazarev <taurus101v@gmail.com>
2013-08-07 23:01:20 +0400

    Redone BaseParser

--- src/gentoid/route/BaseParser.php
+++ src/gentoid/route/BaseParser.php
@@ -3,26 +3,75 @@
 namespace gentoid\route;
 
 
+use gentoid\route\profiles\BasicProfile;
+
 abstract class BaseParser {
 
-	public function __construct(\gentoid\route\profiles\BasicProfile $profile) {
-		//
+	/** @var ExtractorCallbacks */
+	protected $extractor_callbacks;
+
+	/** @var string[] */
+	protected $restriction_exceptions = array();
+
+	/** @var boolean */
+	protected $use_turn_restrictions;
+
+	/** @var BasicProfile */
+	protected $profile;
+
+	/**
+	 * @param ExtractorCallbacks $ec
+	 * @param profiles\BasicProfile $profile
+	 */
+	public function __construct(ExtractorCallbacks $ec, BasicProfile $profile) {
+		$this->extractor_callbacks = $ec;
+		$this->profile = $profile;
+		$this->use_turn_restrictions = true;
+		$this->ReadRestrictionExceptions();
 	}
 
-	abstract public function ParseNode();
-	abstract public function PArseWay();
-	abstract public function reportError();
+	/**
+	 * @param ImportNode $n
+	 */
+	public function ParseNode(ImportNode $n) {
+		try {
+			$this->profile->nodeFunction($n);
+		}
+		catch (\Exception $e) {}
+	}
 
-	protected function ReadUseRestrictionsSetting() {
-		//
+	/**
+	 * @param ExtractionWay $w
+	 */
+	public function ParseWay(ExtractionWay $w) {
+		try {
+			$this->profile->wayFunction($w);
+		}
+		catch (\Exception $e) {}
 	}
 
 	protected function ReadRestrictionExceptions() {
-		//
+		$this->profile->getExceptions($this->restriction_exceptions);
 	}
 
-	protected function ShouldIgnoreRestriction() {
-		//
+	/**
+	 * @param string $except_tag_string
+	 * @return bool
+	 */
+	protected function ShouldIgnoreRestriction($except_tag_string) {
+		if (strlen($except_tag_string) == 0) {
+			return false;
+		}
+
+		$exceptions = preg_split('/[;\s+]/', $except_tag_string);
+		foreach ($exceptions as $exception) {
+			if (in_array($exception, $this->restriction_exceptions)) {
+				return true;
+			}
+		}
+
+		return false;
+
 	}
 
 } 
