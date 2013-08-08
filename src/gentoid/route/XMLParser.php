2f20d38ddbde9e51da28619ff120363a676b954d
Viktor Lazarev <taurus101v@gmail.com>
2013-08-09 00:47:30 +0400

    Migrate from SimpleXML to XMLReader

--- src/gentoid/route/XMLParser.php
+++ src/gentoid/route/XMLParser.php
@@ -7,6 +7,9 @@ use gentoid\utils\LogUtil;
 
 class XMLParser extends BaseParser {
 
+	/**
+	 * @var \XMLReader
+	 */
 	protected $xml;
 
 	/**
@@ -17,7 +20,8 @@ class XMLParser extends BaseParser {
 	public function __construct($filename, ExtractorCallbacks $ec, \gentoid\route\profiles\BasicProfile $profile) {
 		parent::__construct($ec, $profile);
 		LogUtil::info('extracting data from input file '.$filename);
-		$this->xml = new \SimpleXMLElement(file_get_contents($filename));
+		$this->xml = new \XMLReader();
+		$this->xml->open($filename);
 	}
 
 	/**
@@ -29,21 +33,29 @@ class XMLParser extends BaseParser {
 	 * @return bool
 	 */
 	public function parse() {
-		foreach ($this->xml->xpath('node') as $xmlNode) {
-			$n = $this->readXMLNode($xmlNode);
-			$this->parseNode($n);
-			$this->extractor_callbacks->nodeFunction($n);
-		}
+		while ($this->xml->read()) {
+			// 1 is Element
+			if ($this->xml->nodeType != 1) {
+				continue;
+			}
 
-		foreach ($this->xml->xpath('way') as $xmlWay) {
-			$w = $this->readXMLWay($xmlWay);
-			$this->parseWay($w);
-			$this->extractor_callbacks->wayFunction($w);
-		}
+			$name = $this->xml->name;
+			if ($name === null) {
+				continue;
+			}
 
-		if ($this->use_turn_restrictions) {
-			foreach ($this->xml->xpath('relation') as $xmlRelation) {
-				$r = $this->readXMLRestriction($xmlRelation);
+			if ($name == 'node') {
+				$n = $this->readXMLNode();
+				$this->parseNode($n);
+				$this->extractor_callbacks->nodeFunction($n);
+			}
+			elseif ($name == 'way') {
+				$w = $this->readXMLWay();
+				$this->parseWay($w);
+				$this->extractor_callbacks->wayFunction($w);
+			}
+			elseif ($name == 'relation' && $this->use_turn_restrictions) {
+				$r = $this->readXMLRestriction();
 				if ($r->getFromWay() != RawRestrictionContainer::DEFAULT_VALUE) {
 					$this->extractor_callbacks->restrictionFunction($r);
 				}
@@ -54,62 +66,66 @@ class XMLParser extends BaseParser {
 	}
 
 	/**
-	 * @param \SimpleXMLElement $xmlRelation
 	 * @return \gentoid\route\RawRestrictionContainer
 	 */
-	protected function readXMLRestriction(\SimpleXMLElement $xmlRelation) {
+	protected function readXMLRestriction() {
 		$restriction = new RawRestrictionContainer();
 		$except_tag_string = '';
 
-		foreach ($xmlRelation->xpath('tag') as $tag) {
+		if (!$this->xml->isEmptyElement) {
+			$depth = $this->xml->depth;
 
-			$key = null;
-			$value = null;
-			foreach ($attributes = $this->readXMLAttributes($tag) as $k => $v) {
-				if ($k == 'k') {
-					$key = (string)$v;
+			while ($this->xml->read()) {
+				$childType  = $this->xml->nodeType;
+				if ($childType != 1 && $childType != 15) {
+					continue;
 				}
-				elseif ($k == 'v') {
-					$value = (string)$v;
+
+				$childDepth = $this->xml->depth;
+				$childName  = $this->xml->name;
+				if ($childName === null) {
+					continue;
 				}
-			}
-			if ($key && $value) {
-				if ($key == 'restriction' && strpos('only_', $value) !== false) {
-					$restriction->getRestriction()->setIsOnly(true);
+
+				if ($depth == $childDepth && $childType == 15 && $childName == 'relation') {
+					break;
 				}
-				elseif ($key == 'except') {
-					$except_tag_string = $value;
+
+				if ($childType != 1) {
+					continue;
+				}
+
+				if ($childName == 'tag') {
+					$k = $this->xml->getAttribute('k');
+					$v = $this->xml->getAttribute('v');
+					if ($k && $v) {
+						if ($k == 'restriction' && strpos('only_', $v) !== false) {
+							$restriction->getRestriction()->setIsOnly(true);
+						}
+						elseif ($k == 'except') {
+							$except_tag_string = $v;
+						}
+					}
+				}
+				elseif ($childName == 'member') {
+					$ref = $this->xml->getAttribute('ref');
+					if ($ref) {
+						$role = $this->xml->getAttribute('role');
+						$type = $this->xml->getAttribute('type');
+						if ($role == 'to' && $type == 'way') {
+							$restriction->setToWay($ref);
+						}
+						elseif ($role == 'from' && $type == 'way') {
+							$restriction->setFromWay($ref);
+						}
+						elseif ($role == 'via' && $type  == 'node') {
+							$restriction->getRestriction()->setViaNode(new NodeID($ref));
+						}
+					}
 				}
 			}
 		}
 
-		foreach ($xmlRelation->xpath('member') as $member) {
-			$ref  = null;
-			$role = null;
-			$type = null;
-			foreach ($attributes = $this->readXMLAttributes($member) as $k => $v) {
-				if ($k == 'ref') {
-					$ref = (string)$v;
-				}
-				elseif ($k == 'role') {
-					$role = (string)$v;
-				}
-				elseif ($k == 'type') {
-					$type = (string)$v;
-				}
-			}
-			if ($ref) {
-				if ($role == 'to' && $type == 'way') {
-					$restriction->setToWay($ref);
-				}
-				elseif ($role == 'from' && $type == 'way') {
-					$restriction->setFromWay($ref);
-				}
-				elseif ($role == 'via' && $type  == 'node') {
-					$restriction->getRestriction()->setViaNode(new NodeID($ref));
-				}
-			}
-		}
 		// workaround to ignore the restriction
 		if ($this->shouldIgnoreRestriction($except_tag_string)) {
 			$restriction->setFromWay(RawRestrictionContainer::DEFAULT_VALUE);
@@ -119,39 +135,48 @@ class XMLParser extends BaseParser {
 	}
 
 	/**
-	 * @param \SimpleXMLElement $xmlWay
 	 * @return ExtractionWay
 	 */
-	protected function readXMLWay(\SimpleXMLElement $xmlWay) {
+	protected function readXMLWay() {
 		$way = new ExtractionWay();
 
-		foreach ($attributes = $this->readXMLAttributes($xmlWay) as $k => $v) {
-			if ($k == 'id') {
-				$way->setOsmId((string)$v)->setId((string)$v);
-				break;
-			}
-		}
+		if (!$this->xml->isEmptyElement) {
+			$depth = $this->xml->depth;
+			while ($this->xml->read()) {
+				$childType  = $this->xml->nodeType;
+				if ($childType != 1 && $childType != 15) {
+					continue;
+				}
 
-		foreach ($xmlWay->xpath('tag') as $tag) {
-			$key = null;
-			$val = null;
-			foreach ($attributes = $this->readXMLAttributes($tag) as $k => $v) {
-				if ($k == 'k') {
-					$key = (string)$v;
+				$childDepth = $this->xml->depth;
+				$childName  = $this->xml->name;
+				if ($childName === null) {
+					continue;
 				}
-				elseif ($k == 'v') {
-					$val = (string)$v;
-				}
-			}
-			if ($key && $val) {
-				$way->addKeyVal($key, $val);
-			}
-		}
-		foreach ($xmlWay->xpath('nd') as $nd) {
-			foreach ($attributes = $this->readXMLAttributes($nd) as $k => $v) {
-				if ($k == 'ref' && (string)$v) {
-					$way->addPathElement(new NodeID((string)$v));
+
+				if ($depth == $childDepth && $childType == 15 && $childName == 'way') {
+					$id = $this->xml->getAttribute('id');
+					$way->setId($id)->setOsmId($id);
 					break;
+				}
+
+				if ($childType != 1) {
+					continue;
+				}
+
+				if ($childName == 'tag') {
+					$k = $this->xml->getAttribute('k');
+					$v = $this->xml->getAttribute('v');
+
+					if ($k && $v) {
+						$way->addKeyVal($k, $v);
+					}
+				}
+				elseif ($childName == 'nd') {
+					$ref = $this->xml->getAttribute('ref');
+					if ($ref) {
+						$way->addPathElement(new NodeID($ref));
+					}
 				}
 			}
 		}
@@ -160,52 +185,58 @@ class XMLParser extends BaseParser {
 	}
 
 	/**
-	 * @param \SimpleXMLElement $xmlNode
 	 * @return ImportNode
 	 */
-	protected function readXMLNode(\SimpleXMLElement $xmlNode) {
+	protected function readXMLNode() {
 		$node = new ImportNode();
-		foreach ($attributes = $this->readXMLAttributes($xmlNode) as $k => $v) {
-			if ($k == 'lat' && (string)$v) {
-				$node->getCoordinate()->setLat((string)$v);
-			}
-			elseif ($k == 'lon' && (string)$v) {
-				$node->getCoordinate()->setLon((string)$v);
-			}
-			elseif ($k == 'id' && (string)$v) {
-				$node->setNodeId(new NodeID((string)$v));
-			}
+
+		$lat = $this->xml->getAttribute('lat');
+		if ($lat) {
+			$node->getCoordinate()->setLat($lat);
+		}
+		$lon = $this->xml->getAttribute('lon');
+		if ($lon) {
+			$node->getCoordinate()->setLon($lon);
+		}
+		$id = $this->xml->getAttribute('id');
+		if ($id) {
+			$node->setNodeId(new NodeID($id));
 		}
 
-		foreach ($xmlNode->xpath('tag') as $tag) {
-			$key = null;
-			$val = null;
-			foreach ($attributes = $this->readXMLAttributes($tag) as $k => $v) {
-				if ($k == 'k') {
-					$key = (string)$v;
+		if (!$this->xml->isEmptyElement) {
+			$depth = $this->xml->depth;
+			while ($this->xml->read()) {
+				$childType  = $this->xml->nodeType;
+				if ($childType != 1 && $childType != 15) {
+					continue;
 				}
-				elseif ($k == 'v') {
-					$val = (string)$v;
+
+				$childDepth = $this->xml->depth;
+				$childName  = $this->xml->name;
+				if ($childName === null) {
+					continue;
 				}
-			}
-			if ($key && $val) {
-				$node->addKeyVal($key, $val);
+
+				if ($depth == $childDepth && $childType == 15 && $childName == 'node') {
+					break;
+				}
+
+				if ($childType != 1) {
+					continue;
+				}
+
+				if ($childName == 'tag') {
+					$k = $this->xml->getAttribute('k');
+					$v = $this->xml->getAttribute('v');
+
+					if ($k && $v) {
+						$node->addKeyVal($k, $v);
+					}
+				}
 			}
 		}
 
 		return $node;
-	}
-
-	/**
-	 * @param \SimpleXMLElement $xml
-	 * @return array|\SimpleXMLElement
-	 */
-	protected function readXMLAttributes(\SimpleXMLElement $xml) {
-		if ($attributes = $xml->attributes()) {
-			return $attributes;
-		}
-
-		return array();
 	}
 
 }
