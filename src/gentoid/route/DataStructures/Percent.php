3682462c3a072a1646400f63e8d6f49409a1f823
Viktor Lazarev <taurus101v@gmail.com>
2013-08-12 10:50:20 +0400

    Percent class

---
+++ src/gentoid/route/DataStructures/Percent.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * @date: 8/12/13
+ * @uathor: viktor
+ */
+
+namespace gentoid\route\DataStructures;
+
+
+use gentoid\utils\LogUtil;
+
+class Percent {
+
+	protected $maxValue;
+
+	protected $currentValue;
+
+	protected $internalPercent;
+
+	protected $nextThreshold;
+
+	protected $lastPercent;
+
+	protected $step;
+
+	public function __construct($maxValue, $step = 5) {
+		$this->reinit($maxValue, $step);
+	}
+
+	public function reinit($maxValue, $step = 5) {
+		$this->maxValue = $maxValue;
+		$this->currentValue = 0;
+		$this->internalPercent = $maxValue / 100;
+		$this->nextThreshold = $this->internalPercent;
+		$this->lastPercent = 0;
+		$this->step = $step;
+	}
+
+	public function printStatus($currentValue) {
+		if ($currentValue >= $this->nextThreshold) {
+			$this->nextThreshold += $this->internalPercent;
+			$this->printPercent( $this->currentValue / $this->maxValue * 100);
+		}
+		if ($this->currentValue + 1 == $this->maxValue) {
+			LogUtil::infoAsIs(' 100%');
+		}
+	}
+
+	public function printInctrement() {
+		$this->printStatus(++$this->currentValue);
+	}
+
+	public function printAddition($addition) {
+		$this->currentValue += $addition;
+		$this->printStatus($this->currentValue);
+	}
+
+	protected function printPercent($percent) {
+		while ($percent >= $this->lastPercent + $this->step) {
+			$this->lastPercent += $this->step;
+			if ($this->lastPercent % 10 == 0) {
+				LogUtil::infoAsIs(' '.$this->lastPercent.'% ');
+			}
+			else {
+				LogUtil::infoAsIs('.');
+			}
+		}
+	}
+
+}
